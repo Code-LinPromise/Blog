25576dde905e80464670a50ea882aa6c79bda4ee
林鹏涛 <326661445@qq.com>
2022-11-01 13:27:56 +0800

    完成User组件以及AboutMe组件

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import style from "./style.module.scss"
 import {useNavigate} from "react-router-dom"
 
 const Header = (props) => {
-    const {name,details,image}=props
+    const {name,details,image,setIsShowSideNav}=props
     const navigate=useNavigate()
     function GoMain(){
         navigate("/")
@@ -12,7 +12,10 @@ const Header = (props) => {
         <div className={style.background} style={{backgroundImage: `url(${image})`}} >
             <div className={style.top}>
                 <div className={style.topLeft}>
-                    <span className={[style.toggle,"iconfont icon-liebiao"].join(" ")} title="打开列表"></span>
+                    <span className={[style.toggle,"iconfont icon-liebiao"].join(" ")} title="打开列表"
+                          onClick={setIsShowSideNav}
+                    >
+                    </span>
                     <a href="https://github.com/Code-LinPromise" className={style.toggle}
                        target="_blank"
                     >
---
+++ src/components/SideNav/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from "./style.module.scss"
+const SideNav = () => {
+    return (
+        <div className={style.sideNav}>
+
+        </div>
+    );
+};
+
+export default SideNav;
